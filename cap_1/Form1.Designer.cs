c3d384f7e53a32d4ff409089fcbbe55f325d176c
mola666 <2426738828@qq.com>
2022-11-03 10:07:36 +0800

    gugao trap exercise

---
+++ cap_1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace cap_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_init = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_Trap = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_PrfPos = new System.Windows.Forms.Label();
+            this.tb_Pos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_Vel = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_Acc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_Dec = new System.Windows.Forms.TextBox();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_init
+            // 
+            this.btn_init.Location = new System.Drawing.Point(160, 76);
+            this.btn_init.Name = "btn_init";
+            this.btn_init.Size = new System.Drawing.Size(75, 23);
+            this.btn_init.TabIndex = 0;
+            this.btn_init.Text = "初始化";
+            this.btn_init.UseVisualStyleBackColor = true;
+            this.btn_init.Click += new System.EventHandler(this.btn_init_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_Trap
+            // 
+            this.btn_Trap.Location = new System.Drawing.Point(160, 166);
+            this.btn_Trap.Name = "btn_Trap";
+            this.btn_Trap.Size = new System.Drawing.Size(75, 23);
+            this.btn_Trap.TabIndex = 1;
+            this.btn_Trap.Text = "点位运动";
+            this.btn_Trap.UseVisualStyleBackColor = true;
+            this.btn_Trap.Click += new System.EventHandler(this.btn_Trap_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(616, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1轴规划位置";
+            // 
+            // lbl_PrfPos
+            // 
+            this.lbl_PrfPos.AutoSize = true;
+            this.lbl_PrfPos.Location = new System.Drawing.Point(765, 81);
+            this.lbl_PrfPos.Name = "lbl_PrfPos";
+            this.lbl_PrfPos.Size = new System.Drawing.Size(11, 12);
+            this.lbl_PrfPos.TabIndex = 3;
+            this.lbl_PrfPos.Text = "0";
+            this.lbl_PrfPos.Click += new System.EventHandler(this.lbl_PrfPos_Click);
+            // 
+            // tb_Pos
+            // 
+            this.tb_Pos.Location = new System.Drawing.Point(445, 78);
+            this.tb_Pos.Name = "tb_Pos";
+            this.tb_Pos.Size = new System.Drawing.Size(100, 21);
+            this.tb_Pos.TabIndex = 4;
+            this.tb_Pos.Text = "1000";
+            this.tb_Pos.TextChanged += new System.EventHandler(this.tb_Pos_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "目标位置";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(327, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "速度";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // tb_Vel
+            // 
+            this.tb_Vel.Location = new System.Drawing.Point(445, 141);
+            this.tb_Vel.Name = "tb_Vel";
+            this.tb_Vel.Size = new System.Drawing.Size(100, 21);
+            this.tb_Vel.TabIndex = 6;
+            this.tb_Vel.Text = "10";
+            this.tb_Vel.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "加速度";
+            // 
+            // tb_Acc
+            // 
+            this.tb_Acc.Location = new System.Drawing.Point(445, 197);
+            this.tb_Acc.Name = "tb_Acc";
+            this.tb_Acc.Size = new System.Drawing.Size(100, 21);
+            this.tb_Acc.TabIndex = 8;
+            this.tb_Acc.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(327, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "减速度";
+            // 
+            // tb_Dec
+            // 
+            this.tb_Dec.Location = new System.Drawing.Point(445, 256);
+            this.tb_Dec.Name = "tb_Dec";
+            this.tb_Dec.Size = new System.Drawing.Size(100, 21);
+            this.tb_Dec.TabIndex = 10;
+            this.tb_Dec.Text = "1";
+            this.tb_Dec.TextChanged += new System.EventHandler(this.tb_Dec_TextChanged);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(160, 253);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 12;
+            this.btn_stop.Text = "停止运动";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(892, 362);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_Dec);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_Acc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_Vel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_Pos);
+            this.Controls.Add(this.lbl_PrfPos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Trap);
+            this.Controls.Add(this.btn_init);
+            this.Name = "Form1";
+            this.Text = "运动";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_init;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_Trap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_PrfPos;
+        private System.Windows.Forms.TextBox tb_Pos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_Vel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_Acc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_Dec;
+        private System.Windows.Forms.Button btn_stop;
+    }
+}
+
---
+++ cap_1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using gts;
+
+
+namespace cap_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Commandhandler(string cmd,short rtn)
+        {
+            if(rtn != 0)
+            {
+                MessageBox.Show(cmd + " = " + rtn.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            short rtn;
+            //打开运动控制器
+            rtn = mc.GT_Open(0, 1);
+            Commandhandler("gt_open", rtn);
+            //复位
+            rtn = mc.GT_Reset();
+            Commandhandler("GT_Reset", rtn);
+
+            //配置定时器
+            timer1.Interval = 100;
+            timer1.Start();
+
+#if false
+            short rtn;
+            //int rtn;
+            short AXIS = 1;
+
+            mc.TJogPrm jog;
+
+            //轴的状态
+            int sts;
+            short count = 1;
+            uint clock;
+            double prfPos, prfVel;
+
+            //打开运动控制器
+            rtn = mc.GT_Open(0, 1);
+            //Commandhandler("GT_Open", rtn);
+            //复位
+            rtn = mc.GT_Reset();
+            //Commandhandler("GT_Reset", rtn);
+
+            //配置运动控制器
+            mc.GT_LoadConfig(@"F:\C#\temp1\cap_1\cap_1\test1.cfg");
+            //Commandhandler("GT_LoadConfig", rtn);
+            mc.GT_ClrSts(1, 8);
+            //Commandhandler("GT_ClrSts", rtn);
+
+            //将AXIS轴设为Jog模式
+            mc.GT_PrfJog(AXIS);
+            //Commandhandler("GT_PrfJog", rtn);
+
+            jog.acc = 0.0625;
+            jog.dec = 0.0625;
+            jog.smooth = 0.5;
+            mc.GT_SetJogPrm(AXIS, ref jog);
+
+            // 设置 AXIS 轴的目标速度
+            mc.GT_SetVel(AXIS, 500);
+            // 启动 AXIS 轴的运动
+            mc.GT_Update(1 << (AXIS - 1));
+
+#endif
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_init_Click(object sender, EventArgs e)
+        {
+            short rtn;
+            //关闭限位和报警
+            rtn = mc.GT_AlarmOff(1);
+            rtn = mc.GT_LmtsOff(1,-1);
+            //清除状态
+            rtn = mc.GT_ClrSts(1, 1);
+            //使能轴
+            rtn = mc.GT_AxisOn(1);
+
+            //位置清零
+            rtn = mc.GT_ZeroPos(1,1);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            short rtn;
+            int sts;
+            uint clk;
+            double prfPos;
+            rtn = mc.GT_GetSts(1,out sts,1,out clk);
+
+            //取出规划的位置
+            rtn = mc.GT_GetPrfPos(1,out prfPos,1,out clk);
+            //将数字转变为字符串
+            lbl_PrfPos.Text = Convert.ToInt32(prfPos).ToString();
+        }
+
+        private void btn_Trap_Click(object sender, EventArgs e)
+        {
+            short rtn;
+            uint clk;
+            double prfPos;
+
+            //运动速度等显示
+            double vel, acc, dec;
+            int pos;
+            vel = Convert.ToDouble(tb_Vel.Text);
+            acc = Convert.ToDouble(tb_Acc.Text);
+            dec = Convert.ToDouble(tb_Dec.Text);
+            pos = Convert.ToInt32(tb_Pos.Text);
+
+
+
+            mc.TTrapPrm trap;
+            //设置指定轴为点位运动模式
+            rtn = mc.GT_PrfTrap(1);
+            //读取点位运动模式下的运动参数
+            rtn = mc.GT_GetTrapPrm(1,out trap);
+            trap.acc = acc;   //回原点运动的加速度
+            trap.dec = dec;  //回原点运动的减速度
+            trap.smoothTime = 20;  //回原点的平滑时间
+            //设置点位运动模式下的运动参数
+            rtn = mc.GT_SetTrapPrm(1, ref trap);
+
+            //设置速度
+            rtn = mc.GT_SetVel(1, vel);
+            //取出规划的位置
+            rtn = mc.GT_GetPrfPos(1, out prfPos, 1, out clk);
+            //设置到达的位置
+            rtn = mc.GT_SetPos(1, pos);
+            //启动电机
+            rtn = mc.GT_Update(1 << (1 - 1));
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_PrfPos_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tb_Pos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_Dec_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            short rtn;
+            rtn = mc.GT_Stop(1<<(1-1),0);  //后边的0表示，减速度停；如果给1，表示急停
+        }
+    }
+}
+ 
